f384580efe080b261ebd68ab07e08910e82998e2
ZZMasrujeh <43608146+ZZMasrujeh@users.noreply.github.com>
2018-11-05 11:37:17 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const URL = "https://codecyprus.org/th/api/";
+
+//get available quizzes
+HTTP = new XMLHttpRequest();
+HTTP.open("GET", URL + LIST,true);
+HTTP.send();
+HTTP.onload = function () {
+    if (this.status == 200) {
+        let json = this.responseText;
+        let object = JSON.parse(json);
+        // console.log("JSON ");
+        // console.log(object);
+        quizzes= object.treasureHunts;
+        let existingText = document.getElementById("list").innerHTML;
+        for (let i = 0; i <quizzes.length; i++) {
+            //check if quiz is available
+            // existingText += "<li><button onclick='getQuiz(" + i + ")' type='button' style='width: 60%'>" + quizzes[i].name + "</button></li> ";
+        }
+        document.getElementById("list").innerHTML = existingText;     //display the available quizzes
+    }
+};
