7f0ff6f8f893221db498bfd15a2127719a25d0bc
Zacharias <ZZMasrujeh@uclan.ac.uk>
2018-11-05 21:50:55 +0200

    list available quizzes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const URL = "https://codecyprus.org/th/api/";
 const LIST = "list";
 
+var quizzes;
 //get available quizzes
 HTTP = new XMLHttpRequest();
 HTTP.open("GET", URL + LIST,true);
@@ -9,14 +10,29 @@ HTTP.onload = function () {
     if (this.status == 200) {
         let json = this.responseText;
         let object = JSON.parse(json);
-        // console.log("JSON ");
-        // console.log(object);
+        console.log("JSON ");
+        console.log(object);
         quizzes= object.treasureHunts;
-        let existingText = document.getElementById("list").innerHTML;
+        let htmlText = "";
+        document.getElementById("head").innerHTML="<p>Available Quizzes</p>"       //change the "title"
         for (let i = 0; i <quizzes.length; i++) {
-            //check if quiz is available
-            // existingText += "<li><button onclick='getQuiz(" + i + ")' type='button' style='width: 60%'>" + quizzes[i].name + "</button></li> ";
+            //check which quizzes are available and add them to the list
+            htmlText +=
+                "<div class='availableQuiz' >" +
+                "<p>"+quizzes[i].name+"<br>";      //quizz name
+            if (quizzes[i].maxDuration/1000/60) {       //display duration which is not zero
+                htmlText+=
+                    "Duration: "+quizzes[i].maxDuration/1000/60+" minutes<br>";
+            }
+            if (quizzes[i].shuffled) {
+                htmlText+="The questions will be shuffled.<br>";
+            }
+            htmlText+= "by "+quizzes[i].ownerEmail+"<br>"+  //owners email
+                "<button class='button' /onclick='getQuiz(a)'>Start</button>"+   //start button
+                "</p>"+
+                "</div>";
         }
-        document.getElementById("list").innerHTML = existingText;     //display the available quizzes
+        document.getElementById("content").innerHTML = htmlText;     //display the available quizzes
+        document.getElementById("footer").innerText = "";
     }
 };
